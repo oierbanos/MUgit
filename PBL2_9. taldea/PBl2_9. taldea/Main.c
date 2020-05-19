04e39f5ccf969a034ea83be89283ad9899b97bc6
Jon Navaridas <jon.navaridas@alumni.mondragon.edu>
2020-05-19 09:09:21 +0200

    Se han pasado las partes de inicializar a una función independiente

--- PBL2_9. taldea/PBl2_9. taldea/Main.c
+++ PBL2_9. taldea/PBl2_9. taldea/Main.c
@@ -6,45 +6,67 @@
 #define WIDTH 500
 #define HEIGHT 500
 
+int hasieratu(SDL_Window** window, SDL_Renderer** renderer);
+
 // Funtzio orokorra
 int main(int argc, char** argv)
 {
+	SDL_Window* window;
+	SDL_Renderer* renderer;
 	SDL_Event event;
 	int running = 0;
 
-	// Iniciar SDL
-	SDL_Init(SDL_INIT_VIDEO);
+	if (!hasieratu(&window, &renderer)) {
+		SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
 
-	// Crear una ventana con SDL
-	SDL_Window* window = SDL_CreateWindow("Main Window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_OPENGL);
+		// SDL_Delay(20000); Utilizarlo como el sleep
 
-	// Crear un renderizador (Esta sincronizado con la pantalla que aparece al usuario y con el ratio en el que se refresca)
-	SDL_Renderer* render = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-	SDL_SetRenderDrawColor(render, 255, 0, 0, 255);
+		// Loop infinito para mantener la pantalla
+		while (running == 0)
+		{
+			// Mirar a ver si hay un evento
+			while (SDL_PollEvent(&event))
+			{
+				if (event.type == SDL_QUIT) running = 1;
+			}
 
-	// SDL_Delay(20000); Utilizarlo como el sleep
+			// Limpiar la pantalla
+			SDL_RenderClear(renderer);
 
-	// Loop infinito para mantener la pantalla
-	while (running == 0) 
-	{
-		// Mirar a ver si hay un evento
-		while (SDL_PollEvent(&event)) 
-		{ 
-			if (event.type == SDL_QUIT) running = 1;
+			// Dibujar algo
+
+			// Mostrar lo que se ha dibujado
+			SDL_RenderPresent(renderer);
 		}
+	}
+	// Liberar informacion y terminar
+	if (renderer) SDL_DestroyRenderer(renderer);
+	if (window) SDL_DestroyWindow(window);
+	SDL_QUIT;
+	return 0;
+}
 
-		// Limpiar la pantalla
-		SDL_RenderClear(render);
-		// Dibujar algo
-
-		// Mostrar lo que se ha dibujado
-		SDL_RenderPresent(render);
+int hasieratu(SDL_Window** window, SDL_Renderer** renderer)
+{
+	// Iniciar SDL comprobando errores
+	if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+		fprintf(stderr, "SDL ez da hasieratu: %s\n", SDL_GetError());
+		return 1;
 	}
 
-	// Liberar informacion y terminar
-	SDL_DestroyRenderer(render);
-	SDL_DestroyWindow(window);
-	SDL_QUIT;
+	// Crear una ventana con SDL comprobando error
+	*window = SDL_CreateWindow("Main Window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_OPENGL);
+	if (!window) {
+		fprintf(stderr, "Ez da leihoa sortu.\n");
+		return 1;
+	}
+
+	// Crear un renderizador (Esta sincronizado con la pantalla que aparece al usuario y con el ratio en el que se refresca)
+	*renderer = SDL_CreateRenderer(*window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+	if (!renderer) {
+		fprintf(stderr, "Could not create renderer\n");
+		return 1;
+	}
 
 	return 0;
 }
