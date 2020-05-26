7ba73eab0348a9ff46a528f5ec95c134dbd3964c
Jon Navaridas <jon.navaridas@alumni.mondragon.edu>
2020-05-26 10:15:25 +0200

    Habemus botones

---
+++ Test menu/PBl2_9. taldea/Main.c
@@ -0,0 +1,44 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "Text.h"
+#include "SDL.h"
+#include "SDL_ttf.h"
+
+#define WIDTH 1109
+#define HEIGHT 680
+
+// Funtzio orokorra
+int main(int argc, char** argv)
+{
+	char fileName[128] = "", mapName[128] = "";
+	int running = 0, egoera;
+	SDL_Renderer* renderer;
+	SDL_Window* window;
+	SDL_Event ebentu;
+	DIM mapDim;
+
+	mapDim.height = 0;
+	mapDim.width = 0;
+
+	if (!hasieratu(&window, &renderer, WIDTH, HEIGHT, "MUgit") && TTF_Init() == 0) {
+		atexit(TTF_Quit);
+		textuaGaitu(1);
+
+		while (running == 0) { // Loop infinito para mantener la pantalla
+			while ((egoera = SDL_PollEvent(&ebentu)) == 1 && running == 0) {
+				SDL_RenderClear(renderer); // Limpiar
+				if (egoera == 1) running = aukeraMenu(ebentu, fileName, mapName, mapDim);
+				SDL_RenderPresent(renderer); // Refrescar
+				SDL_UpdateWindowSurface(window); // Actualizar pantalla
+			}
+		}
+	}
+	if (renderer) SDL_DestroyRenderer(renderer);
+	if (window) SDL_DestroyWindow(window);
+	SDL_StopTextInput();
+	SDL_Quit(); // Necesario poner esto, sino en Linux da warning
+
+	return 0;
+}
